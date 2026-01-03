d7b2387260e332db7acd8d6e27dd07c87f7a8290
Suchit Mahi <suchitmahi14@gmail.com>
2026-01-03 20:05:29 +0530

    added playGame function

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ let compScore = 0;
 
 const choices = document.querySelectorAll(".choice");
 
+
+
+const playGame = (userChoice) => {
+    console.log("user choice =", userChoice);
+};
+
 choices.forEach((choice) => {
     choice.addEventListener("click" , () => {
         const userChoice = choice.getAttribute("id");
         console.log(userChoice ,"was clicked");
+        playGame(userChoice);
     });
 });
