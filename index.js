9d3e1ed4ba79d33343b1c5abf986b29cc3adc31a
Suchit Mahi <suchitmahi14@gmail.com>
2026-01-03 20:13:54 +0530

    added genCompChoice function to the js

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ let compScore = 0;
 
 const choices = document.querySelectorAll(".choice");
 
-
+const genCompChoice = () => {
+    const options = ["rock", "paper" , "scissors"];
+    const randomIdx = Math.floor(Math.random() * 3);
+    return options[randomIdx];
+}
 
 const playGame = (userChoice) => {
     console.log("user choice =", userChoice);
