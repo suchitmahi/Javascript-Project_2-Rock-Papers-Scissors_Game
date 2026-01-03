744cc83a261811323682313e92d710ad4c4f32ac
Suchit Mahi <suchitmahi14@gmail.com>
2026-01-03 19:55:08 +0530

    added even listener to rock, paper, scissors

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ let useScore = 0;
 let compScore = 0;
 
 const choices = document.querySelectorAll(".choice");
+
+choices.forEach((choice) => {
+    choice.addEventListener("click" , () => {
+        const choiceId = choice.getAttribute("id");
+        console.log(choiceId ,"was clicked");
+    });
+});
