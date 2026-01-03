3a996ad129f60481c0f16880f493fa306e3394de
Suchit Mahi <suchitmahi14@gmail.com>
2026-01-03 21:18:25 +0530

    document.querySelector to #msg

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let useScore = 0;
 let compScore = 0;
 
 const choices = document.querySelectorAll(".choice");
+const msg = document.querySelector("#msg");
 
 const genCompChoice = () => {
     const options = ["rock", "paper" , "scissors"];
